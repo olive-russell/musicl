6918a66e97a9dc4aff325c16fa4d3ff9d2d80fac
Alexander Russell <alexander.russell4@pm.me>
2026-06-06 14:42:02 +1000

    Implement init

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,6 +1,19 @@
 use anyhow::Result;
 
 pub fn handle() -> Result<()> {
-    println!("Initialising");
+    // If .musicl directory exists, report and exit
+    if std::path::Path::new(".musicl").exists() {
+        print!("Library already exists in working directory.");
+        std::process::exit(1);
+    }
+
+    // Create .musicl directory
+    std::fs::create_dir(".musicl")?;
+
+    // Create status file
+    std::fs::File::create(".musicl/status")?;
+
+    // Report back
+    print!("Library created in working directory.");
     Ok(())
 }
