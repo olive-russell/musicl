0db1c378f54979ad6ad31e9a2794e2fbca08d815
Alexander Russell <alexander.russell4@pm.me>
2026-06-06 12:20:05 +1000

    Tidy

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,8 @@ pub mod schema;
 use std::path::PathBuf;
 
 use diesel::prelude::*;
-// use dotenvy::dotenv;
-// use std::env;
 
 pub fn establish_connection(db: PathBuf) -> SqliteConnection {
-    // dotenv().ok();
-
-    // let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    // let database_url = "C:/Users/alexa/Desktop/Garage/Workbench/2025-09-26_MusicCliInRust/music.db";
     SqliteConnection::establish(&db.to_string_lossy().into_owned())
         .unwrap_or_else(|_| panic!("Error connecting to {}", &db.to_string_lossy().into_owned()))
 }
--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,30 @@ use musicl::establish_connection;
 use ctx::Ctx;
 
 fn main() -> Result<()> {
+    // Parse CLI
     let cli = Cli::parse();
 
+    // Calculate database path from working directory
     let mut db_path = std::env::current_dir()?;
     db_path.push(".musicl");
     db_path.push("musicl.db");
 
+    // Dispatch subcommand (init or something else)
     match cli.command {
         Commands::Init{} => commands::init::handle()?,
         _ => {
+            // Test database path
+            if !db_path.exists() {
+                println!("Database not found at: {:?}. Please run 'musicl init' first.", db_path);
+                std::process::exit(1);
+            }
+
+            // Establish database connection
             let mut ctx = Ctx {
                 connection: &mut establish_connection(db_path),
             };
+
+            // Dispatch subcommand
             match cli.command {
                 Commands::Add{path} => commands::add::handle(&mut ctx, path)?,
                 Commands::Archive{path} => commands::archive::handle(&mut ctx, path)?,
