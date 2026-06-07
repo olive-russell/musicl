df12479a6f1bef13c9b2e417d755f9eff2cd8c02
Alexander Russell <alexander.russell4@pm.me>
2026-06-07 17:56:43 +1000

    Test add subcommand

---
+++ tests/add.rs
@@ -0,0 +1,139 @@
+use std::{fs, path::PathBuf};
+use chrono::Local;
+use tempfile::tempdir;
+
+use crate::common::all_but_last_line;
+
+mod common;
+
+#[test]
+fn add_rejects_bad_metadata() {
+    let temp_dir = tempdir().unwrap();
+
+    // Create demo library, place a bad file at root
+    common::make_tiny_library(&temp_dir);
+    let bad_metadata_path = PathBuf::from("tests/data/bad_metadata_file1/Parliament/Funkentelechy Vs. The Placebo Syndrome/1-03 Wizard Of Finance.mp3");
+    fs::copy(&bad_metadata_path, &temp_dir).expect("Failed to copy bad file in to root");
+    
+    // Run add subcommand
+    common::run_musicl_get_stdout(&["add", bad_metadata_path.to_str().expect("Failed to strify")], &temp_dir);
+
+    // Assert file not moved, status not changed
+    assert!(bad_metadata_path.is_file());
+    common::assert_file_contents_eq(temp_dir.path().join(".musicl/status"), PathBuf::from("tests/data/tiny_library/.musicl/status"))
+}
+
+fn rejects_non_music_file(subcommand: &str) {
+    let temp_dir = tempdir().unwrap();
+
+    // Determine where to place file
+    let destination = match subcommand {
+        "add" => PathBuf::from(temp_dir.path()),
+        "archive" => temp_dir.path().join("library"),
+        "unarchive" => temp_dir.path().join("archive"),
+        "remove" => temp_dir.path().join("library"),
+        _ => panic!("Unimplemented sub-command"),
+    };
+
+    // Create demo library, place a bad file at root
+    common::make_small_library(&temp_dir);
+    let demo_non_music_file_path = PathBuf::from("tests/data/non_music_file.txt");
+    let non_music_file_path = destination.join("non_music_file.txt");
+    fs::copy(&demo_non_music_file_path, &non_music_file_path).expect("Failed to copy non-music file file in to root");
+    
+    // Run add subcommand
+    common::run_musicl_get_stdout(&[subcommand, non_music_file_path.to_str().expect("Failed to strify")], &temp_dir);
+
+    // Assert file not moved, status not changed
+    assert!(non_music_file_path.is_file());
+    common::assert_file_contents_eq(temp_dir.path().join(".musicl/status"), PathBuf::from("tests/data/small_library/.musicl/status"))
+}
+
+#[test]
+fn add_rejects_non_music_file() {
+    rejects_non_music_file("add");
+}
+
+#[test]
+fn add_moves_good_file() {
+    let temp_dir = tempdir().unwrap();
+
+    // Create demo library, place a good file at root
+    common::make_tiny_library(&temp_dir);
+    let good_file_path =  PathBuf::from("tests/data/correctly_located_file1/Pavement/Brighten the Corners/01 Stereo.mp3");
+    fs::copy(&good_file_path, &temp_dir).expect("Failed to copy good file in to library");
+    
+    // Run add subcommand
+    common::run_musicl_get_stdout(&["add", good_file_path.to_str().expect("Failed to strify")], &temp_dir);
+
+    // Assert file moved, status changed
+    assert!(!good_file_path.is_file());
+    common::assert_file_contents_ne(temp_dir.path().join(".musicl/status"), PathBuf::from("tests/data/tiny_library/.musicl/status"))
+}
+
+#[test]
+fn add_updates_status() {
+    let temp_dir = tempdir().unwrap();
+
+    // Create demo library, place a good file at root
+    common::make_tiny_library(&temp_dir);
+    let good_file_path =  PathBuf::from("tests/data/correctly_located_file1/Pavement/Brighten the Corners/01 Stereo.mp3");
+    fs::copy(&good_file_path, &temp_dir.path().join("stereo.mp3")).expect("Failed to copy good file in to library");
+    
+    // Run add subcommand
+    common::run_musicl_get_stdout(&["add", good_file_path.to_str().expect("Failed to strify")], &temp_dir);
+
+    // Assert file moved, status changed
+    let correct_status = format!("{},USMTD9719701,Add", Local::now().format("%Y-%m-%d"));
+    assert!(!good_file_path.is_file());
+    assert_eq!(common::last_line(&temp_dir.path().join(".musicl/status")), correct_status);
+}
+
+#[test]
+fn add_creates_library_folder() {
+    let temp_dir = tempdir().unwrap();
+
+    // Create demo library, place a good file at root
+    common::make_empty_library(&temp_dir);
+    let good_file_path =  PathBuf::from("tests/data/correctly_located_file1/Pavement/Brighten the Corners/01 Stereo.mp3");
+    fs::copy(&good_file_path, &temp_dir.path().join("stereo.mp3")).expect("Failed to copy good file in to library");
+    
+    // Run add subcommand
+    common::run_musicl_get_stdout(&["add", good_file_path.to_str().expect("Failed to strify")], &temp_dir);
+
+    // Assert library folder exists now
+    assert!(temp_dir.path().join("library").is_dir());
+}
+
+#[test]
+fn add_correctly_selects_new_location() {
+    let temp_dir = tempdir().unwrap();
+
+    // Create demo library, place a good file at root
+    common::make_tiny_library(&temp_dir);
+    let good_file_path =  PathBuf::from("tests/data/correctly_located_file1/Pavement/Brighten the Corners/01 Stereo.mp3");
+    fs::copy(&good_file_path, &temp_dir.path().join("stereo.mp3")).expect("Failed to copy good file in to library");
+    
+    // Run add subcommand
+    common::run_musicl_get_stdout(&["add", good_file_path.to_str().expect("Failed to strify")], &temp_dir);
+
+    // Assert file moved, status changed
+    let correct_location = temp_dir.path().join("library/Pavement/Brighten the Corners/01 Stereo.mp3");
+    assert!(correct_location.is_file());
+}
+
+#[test]
+fn add_only_appends_to_status() {
+    let temp_dir = tempdir().unwrap();
+
+    // Create demo library, place a good file at root
+    common::make_tiny_library(&temp_dir);
+    let good_file_path =  PathBuf::from("tests/data/correctly_located_file1/Pavement/Brighten the Corners/01 Stereo.mp3");
+    fs::copy(&good_file_path, &temp_dir.path().join("stereo.mp3")).expect("Failed to copy good file in to library");
+  
+    // Run add subcommand
+    common::run_musicl_get_stdout(&["add", good_file_path.to_str().expect("Failed to strify")], &temp_dir);
+
+    // Assert file moved, status identical besides last line
+    assert_eq!(fs::read_to_string("tests/data/tiny_library/.musicl/status").expect("Failed to read demo status"), all_but_last_line(&temp_dir.path().join(".musicl/status")));
+}
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 use assert_cmd::{Command, assert::Assert};
 use fs_extra::dir;
@@ -25,9 +25,11 @@ pub fn run_musicl_get_stdout(args: &[&str], temp_dir: &TempDir) -> String {
     String::from_utf8(output.stdout).expect("Failed to parse output to string")
 }
 
-// pub fn make_empty_library(temp_dir: &TempDir) {
-//     fs::copy("tests/data/tiny_library/.musicl", &temp_dir).expect("Failed to create demo .musicl directory.");
-// }
+pub fn make_empty_library(temp_dir: &TempDir) {
+    let mut options = dir::CopyOptions::new();
+    options.content_only = true;
+    dir::copy("tests/data/empty_library", temp_dir.path(), &options).expect("Failed to create demo empty library.");
+}
 
 pub fn make_tiny_library(temp_dir: &TempDir) {
     let mut options = dir::CopyOptions::new();
@@ -68,7 +70,7 @@ pub fn copy_bad_metadata_file1(temp_dir: &TempDir, sub_library: &str) -> PathBuf
     // Check path has the sub library
     let library_path = temp_dir.path().join(sub_library);
     if !library_path.exists() {
-        print!("Library does not exist in test directory.");
+        println!("{sub_library} does not exist in test directory.");
         std::process::exit(1);
     }
 
@@ -79,4 +81,34 @@ pub fn copy_bad_metadata_file1(temp_dir: &TempDir, sub_library: &str) -> PathBuf
 
     // Returne the hardcoded path to the mp3
     temp_dir.path().join("library").join("Parliament/Funkentelechy Vs. The Placebo Syndrome/1-03 Wizard Of Finance.mp3")
+}
+
+pub fn assert_file_contents_eq(path1: PathBuf, path2: PathBuf) {
+    let actual = fs::read_to_string(path1).expect("Couldn't read file at path1.");
+    let expected = fs::read_to_string(path2).expect("Couldn't read file at path2");
+    assert_eq!(actual, expected);
+}
+
+pub fn assert_file_contents_ne(path1: PathBuf, path2: PathBuf) {
+    let actual = fs::read_to_string(path1).expect("Couldn't read file at path1.");
+    let expected = fs::read_to_string(path2).expect("Couldn't read file at path2");
+    assert_ne!(actual, expected);
+}
+
+pub fn last_line(path: &PathBuf) -> String {
+    fs::read_to_string(path)
+        .expect("Failed to read file")
+        .lines()
+        .last()
+        .unwrap_or("")
+        .to_string()
+}
+
+pub fn all_but_last_line(path: &PathBuf) -> String {
+    let content = fs::read_to_string(path)
+        .expect("Failed to read file");
+
+    let mut lines: Vec<_> = content.lines().collect();
+    lines.pop();
+    lines.join("\n")
 }
