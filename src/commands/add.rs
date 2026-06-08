ade290b4b880c5c2ab0721d711893ea40d455e99
Alexander Russell <alexander.russell4@pm.me>
2026-06-08 22:02:27 +1000

    Scaffold add subcommand

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -1,9 +1,48 @@
 use anyhow::Result;
+use glob::glob;
+use musicl::is_music_file;
 use std::path::PathBuf;
 
 use crate::ctx::Ctx;
 
 pub fn handle(ctx: &mut Ctx, path: PathBuf) -> Result<()> {
-    println!("Adding: {:?} to {:?}", path, ctx.status_path);
+    // Iterate files
+    for file_path in glob(path.to_str().unwrap()).expect("Failed to glob path") {
+        match file_path {
+            Ok(file_path) => {
+                print!("{}: ", file_path.to_str().unwrap());
+                // Check file is not in library or archive
+                if (path_in_library(file_path) || path_in_archive(file_path)) {
+                    println!("Not added. Path is within library/archive.")
+                    continue;
+                }
+                
+                // Check that it is a music file
+                if (!is_music_file(file_path)) {
+                    println!("Not added. Is not a music file.");
+                    continue;
+                }
+
+                // Check all required metadata fields are there
+                let missing_metadata = missing_metadata(file_path);
+                if missing_metadata {
+                    println!("Not added. Is not a music file.");
+                    continue;
+                }
+
+                // Check ISRC available
+                if isrc_in_library(file_path) {
+                    println!("Not added. ISRC already in use.");
+                    continue;
+                }
+
+                // Move file in and add to status
+                move_music(file_path, "library");
+                println!("Added.");
+                println!("{:?}", path.display())
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
     Ok(())
 }
---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub fn get_metadata() {
+
+}
+
+pub fn has_sufficient_metadata() {
+
+}
+
+pub fn is_music_file() {
+
+}
+
+pub fn get_correct_location() {
+
+}
+
+pub fn update_status() {
+    
+}
+
