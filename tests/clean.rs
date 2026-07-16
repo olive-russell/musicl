b31173cbcc249217c87ddeb743531e802f7bc6ea
Alexander Russell <alexander.russell4@pm.me>
2026-07-16 13:19:36 +1000

    Tests for #3

--- tests/clean.rs
+++ tests/clean.rs
@@ -73,3 +73,21 @@ fn clean_leaves_correctly_located_file() {
     assert!(temp_dir.path().join("library/Talking Heads/Naked/05 (Nothing But) Flowers.mp3").exists());
     assert!(temp_dir.path().join("archive/Jacques Dutronc/Best Of Jacques Dutronc/05 Les gens sont fous, les temps sont flous (Remastered).mp3").exists());
 }
+
+#[test]
+fn clean_survives_bad_metadata() {
+        let temp_dir = tempdir().unwrap();
+
+    // Create demo library
+    common::make_small_library(&temp_dir);
+
+    // Add file with bad metadata
+    let bad_metadata_file_path = "tests/data/bad_metadata_file1/Parliament/Funkentelechy Vs. The Placebo Syndrome/1-03 Wizard Of Finance.mp3";
+    let destination = temp_dir.path().join("library/Parliament/Funkentelechy Vs. The Placebo Syndrome/1-03 Wizard Of Finance.mp3");
+    let parent_dir = destination.parent().expect("Failed to get parent directory for bad metadata file");
+    std::fs::create_dir_all(&parent_dir).expect("Failed to create parent directories for bad metadata file");
+    std::fs::copy(bad_metadata_file_path, &destination).expect("Failed to copy bad metadata file");
+
+    // Run clean subcommand
+    common::run_musicl(&["clean"], &temp_dir).success();
+}
